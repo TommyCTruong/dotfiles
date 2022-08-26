a6514036fa60c6da0df690b54d348e3c9691b5a6
Tam C Truong <tam@cuti.net>
2022-08-26 13:38:10 -0500

    Update dot_zsh/scripts/apps_pacman.txt Update dot_zsh/scripts/executable_setup.sh

--- dot_zsh/scripts/executable_setup.sh
+++ dot_zsh/scripts/executable_setup.sh
@@ -1,7 +1,12 @@
 #!/bin/zsh
-#
+### Update system
 sudo pacman -Syu --noconfirm
+
+### Install slick greeter for lightd display manager ##
+sudo pacman -S lightdm-slick-greeter lightdm-settings
+### Install chezmoi
 sudo pacman -S chezmoi
+
 chezmoi init --apply git@github.com:tommyctruong/dotfiles.git
 exec zsh
 mkdir ~/git/
@@ -9,3 +14,11 @@ cd ~/git/
 git clone git@github:tommyctruong/wiki.git
 chsh -s /bin/zsh
 ./install_pacman.sh
+
+# set up screensaver ##
+
+mkdir -p ~/.config/systemd/user/
+
+cp ./xscreensaver.service ~/.config/systemd/user/
+
+systemctl --user enable xscreensaver
