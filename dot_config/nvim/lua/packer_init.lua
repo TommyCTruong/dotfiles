e34a58108ea80613bf36c3052605076c5b1051d2
Tam C Truong <tam@cuti.net>
2022-08-31 09:49:05 -0500

    Update dot_config/nvim/lua/core/options.lua Update dot_config/nvim/lua/packer_init.lua Update dot_config/nvim/plugin/packer_compiled.lua

--- dot_config/nvim/lua/packer_init.lua
+++ dot_config/nvim/lua/packer_init.lua
@@ -63,10 +63,18 @@ return packer.startup(function(use)
     'vimwiki/vimwiki',
     config = function()
       vim.g.vimwiki_global_ext = 0
+      vim.cmd([[
+      hi VimwikiHeader1 guifg=#FF0000
+      hi VimwikiHeader2 guifg=#00FF00
+      hi VimwikiHeader3 guifg=#0000FF
+      hi VimwikiHeader4 guifg=#FF00FF
+      hi VimwikiHeader5 guifg=#00FFFF
+      hi VimwikiHeader6 guifg=#FFFF00
+]])
       vim.g.vimwiki_list = {
         {
           path = '~/wiki/',
-          syntax = 'markdown',
+          syntax = 'default',
           ext  = '.wiki',
         }
       }
--- dot_config/nvim/plugin/packer_compiled.lua
+++ dot_config/nvim/plugin/packer_compiled.lua
@@ -187,7 +187,7 @@ _G.packer_plugins = {
     url = "https://github.com/preservim/tagbar"
   },
   vimwiki = {
-    config = { "\27LJ\2\nƒ\1\0\0\3\0\5\0\v6\0\0\0009\0\1\0)\1\0\0=\1\2\0006\0\0\0009\0\1\0004\1\3\0005\2\4\0>\2\1\1=\1\3\0K\0\1\0\1\0\3\bext\n.wiki\tpath\f~/wiki/\vsyntax\rmarkdown\17vimwiki_list\23vimwiki_global_ext\6g\bvim\0" },
+    config = { "\27LJ\2\nü\2\0\0\3\0\a\0\0156\0\0\0009\0\1\0)\1\0\0=\1\2\0006\0\0\0009\0\3\0'\2\4\0B\0\2\0016\0\0\0009\0\1\0004\1\3\0005\2\6\0>\2\1\1=\1\5\0K\0\1\0\1\0\3\vsyntax\fdefault\tpath\f~/wiki/\bext\n.wiki\17vimwiki_listé\1      hi VimwikiHeader1 guifg=#FF0000\n      hi VimwikiHeader2 guifg=#00FF00\n      hi VimwikiHeader3 guifg=#0000FF\n      hi VimwikiHeader4 guifg=#FF00FF\n      hi VimwikiHeader5 guifg=#00FFFF\n      hi VimwikiHeader6 guifg=#FFFF00\n\bcmd\23vimwiki_global_ext\6g\bvim\0" },
     loaded = true,
     path = "/home/tam/.local/share/nvim/site/pack/packer/start/vimwiki",
     url = "https://github.com/vimwiki/vimwiki"
@@ -195,10 +195,6 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: vimwiki
-time([[Config for vimwiki]], true)
-try_loadstring("\27LJ\2\nƒ\1\0\0\3\0\5\0\v6\0\0\0009\0\1\0)\1\0\0=\1\2\0006\0\0\0009\0\1\0004\1\3\0005\2\4\0>\2\1\1=\1\3\0K\0\1\0\1\0\3\bext\n.wiki\tpath\f~/wiki/\vsyntax\rmarkdown\17vimwiki_list\23vimwiki_global_ext\6g\bvim\0", "config", "vimwiki")
-time([[Config for vimwiki]], false)
 -- Config for: gitsigns.nvim
 time([[Config for gitsigns.nvim]], true)
 try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
@@ -207,6 +203,10 @@ time([[Config for gitsigns.nvim]], false)
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
 time([[Config for nvim-autopairs]], false)
+-- Config for: vimwiki
+time([[Config for vimwiki]], true)
+try_loadstring("\27LJ\2\nü\2\0\0\3\0\a\0\0156\0\0\0009\0\1\0)\1\0\0=\1\2\0006\0\0\0009\0\3\0'\2\4\0B\0\2\0016\0\0\0009\0\1\0004\1\3\0005\2\6\0>\2\1\1=\1\5\0K\0\1\0\1\0\3\vsyntax\fdefault\tpath\f~/wiki/\bext\n.wiki\17vimwiki_listé\1      hi VimwikiHeader1 guifg=#FF0000\n      hi VimwikiHeader2 guifg=#00FF00\n      hi VimwikiHeader3 guifg=#0000FF\n      hi VimwikiHeader4 guifg=#FF00FF\n      hi VimwikiHeader5 guifg=#00FFFF\n      hi VimwikiHeader6 guifg=#FFFF00\n\bcmd\23vimwiki_global_ext\6g\bvim\0", "config", "vimwiki")
+time([[Config for vimwiki]], false)
 if should_profile then save_profiles() end
 
 end)
