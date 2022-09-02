fff5763079c40f3db0a4b7e3a17e899b7f63cd66
Tam C Truong <tam@cuti.net>
2022-09-02 09:53:55 -0500

    Update dot_zsh/dot_zshrc Add dot_zsh/executable_functions.sh

---
+++ dot_zsh/executable_functions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+google() {
+    search=""
+    echo "Googling: $@"
+    for term in $@; do
+        search="$search%20$term"
+    done
+    xdg-open "http://www.google.com/search?q=$search"
+}
