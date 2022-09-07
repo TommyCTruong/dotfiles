dfece046ea5b909b0786790dfa045d4e52730cbe
Tam C Truong <tam@cuti.net>
2022-09-07 08:13:30 -0500

    Update dot_zsh/scripts/apps_pacman.txt Update dot_zsh/scripts/executable_setup.sh

--- dot_zsh/scripts/executable_setup.sh
+++ dot_zsh/scripts/executable_setup.sh
@@ -1,11 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 ### Update system
 sudo pacman -Syu --noconfirm
 
 ### Install slick greeter for lightd display manager ##
-sudo pacman -S lightdm-slick-greeter lightdm-settings
+sudo pacman -S lightdm-slick-greeter lightdm-settings --needed --noconfirm
 ### Install chezmoi
-sudo pacman -S chezmoi
 
 chezmoi init --apply git@github.com:tommyctruong/dotfiles.git
 exec zsh
@@ -22,3 +21,6 @@ mkdir -p ~/.config/systemd/user/
 cp ./xscreensaver.service ~/.config/systemd/user/
 
 systemctl --user enable xscreensaver
+
+#enable cronie
+sudo systemctl --now enable cronie
