6fb345bc71e2c707bc7b5c35faa8bff29ede259e
Tam C Truong <tam@cuti.net>
2022-09-21 12:56:33 -0500

    Update dot_config/alacritty/alacritty.yml Update dot_zsh/scripts/executable_set_bridge.sh

--- dot_zsh/scripts/executable_set_bridge.sh
+++ dot_zsh/scripts/executable_set_bridge.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/zsh
 
+sudo pacman -S qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+
 sudo cp ~/.zsh/bk/network/* /etc/systemd/network/
 
 # disable network manager
 sudo systemctl disable NetworkManager
 
 # enable systemd network
-sudo systemctl systemd-networkd
+sudo systemctl enable --now systemd-networkd
