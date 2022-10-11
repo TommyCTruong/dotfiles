7168d86c36bff4df151e5db9c84e3022e0942796
Tam C Truong <tam@cuti.net>
2022-10-11 09:08:47 -0500

    Add dot_config/private_fish/completions/.keep Add dot_config/private_fish/conf.d/.keep Add dot_config/private_fish/config.fish Add dot_config/private_fish/fish_variables Add dot_config/private_fish/functions/fish_user_key_bindings.fish

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,14 @@
+### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/.cargo/bin $fish_user_paths
+
+### EXPORT ###
+set fish_greeting                                 # Supresses fish's intro message
+set TERM "xterm-256color"                         # Sets the terminal type
+set -gx EDITOR nvim
+
+source ~/.aliases
+zoxide init fish | source
+starship init fish | source
